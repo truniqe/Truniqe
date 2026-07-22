18091f02b888bd6ef364077616e4156391513df2
truniqe <truniqe@gmail.com>
2026-07-22 17:29:07 +0530

    Update admin-properties.js

--- js/admin/admin-properties.js
+++ js/admin/admin-properties.js
@@ -11,7 +11,7 @@ let pendingDeleteName = null;
 
 async function init() {
   await initAuth();
-  // if (!isAdmin()) { requireAdmin(); return; }
+  if (!isAdmin()) { requireAdmin(); return; }
   onAuthChange(updateAdminNav);
   setupSidebarToggle();
   setupDeleteModal();
